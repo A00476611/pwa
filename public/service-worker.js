999ae6701ced4733a79b9639177ad2a120057410
A00476611 <Dshon.Henry@smu.ca>
2024-02-07 10:41:30 -0400

    adds caching

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,13 +1,28 @@
+const CACHE_NAME = 'todo-cache-v1';
+
 self.addEventListener('activate', event => {
     console.log("Service worker is working")
 })
 
-var CACHE_NAME = 'my-pwa-cache-v1';
-var urlsToCache = [
-    '/',
-    '/styles/styles.css',
-    '/script/webpack-bundle.js'
-];
+const putInCache = async (request, response) => {
+    const cache = await caches.open(CACHE_NAME);
+    await cache.put(request, response);
+};
+
+const cacheFirst = async (request) => {
+    const responseFromCache = await caches.match(request);
+    if (responseFromCache) {
+        return responseFromCache;
+    }
+    const responseFromNetwork = await fetch(request);
+    putInCache(request, responseFromNetwork.clone());
+    return responseFromNetwork;
+};
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(cacheFirst(event.request));
+});
+
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
